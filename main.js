8876a823fcc6ca69043d4455a36fd46640d1445c
gong-weitao <1395051988@qq.com>
2021-03-04 17:06:15 +0800

    解决浮点舍入误差的问题

--- main.js
+++ main.js
@@ -17,8 +17,25 @@ $(() => {
     let p = (w + h) * 2;
     let a = w * h;
 
+    // 解决浮点舍入误差的问题
+    function roundFractional(x, n) {
+      return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+    }
+    let n1,n2;
+    try{
+      n1 = w.toString().split('.')[1].length
+    }catch(e){
+      n1 = 0
+    }
+    try{
+      n2 = h.toString().split('.')[1].length
+    }catch(e){
+      n2 = 0
+    }
+    
     // output
-    $perimeter.val(p);
-    $area.val(a);
+    $perimeter.val(roundFractional(p,Math.max(n1,n2)));
+    $area.val(roundFractional(a,n1+n2));
+    
   })
 })
